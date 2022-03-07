f12066393ec27ca5a4077868dac7084fdc3be275
Oznur Celebi <o.ozgumus17@gmail.com>
2022-03-07 03:31:49 +0100

    first commit

---
+++ apiTodo/serializers.py
@@ -0,0 +1,16 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    todo_detail =serializers.HyperlinkedIdentityField(view_name ='todo-detail')
+    class Meta:
+        model = Todo
+        fields = [
+            "id",
+            "task",
+            "description",
+            "done",
+            "todo_detail",
+        ]
